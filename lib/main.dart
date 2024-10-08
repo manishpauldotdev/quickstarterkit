2efc030320b15625a797df8455ba7ecdf371a4b9
Manish Paul <manishpaul.dev@gmail.com>
2024-10-08 08:54:27 +0530

    added screenutil and riverpod in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
+    runApp(
+      const ProviderScope(
+        child: MainApp(),
+      ),
+    );
+  });
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +20,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Quick Starter Kit'),
-        ),
-      ),
+    return ScreenUtilInit(
+      // Update the width and height according to your design
+      designSize: const Size(393, 852),
+      useInheritedMediaQuery: true,
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          // routerConfig: router,
+          debugShowCheckedModeBanner: false,
+          title: 'Quick Starter Kit',
+          theme: ThemeData(
+            useMaterial3: true,
+            pageTransitionsTheme: const PageTransitionsTheme(
+              builders: {
+                TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+                TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
